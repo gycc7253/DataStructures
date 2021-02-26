e2b82e76058268e475bcf935986a34496a87dfd2
gycc7253 <guo7253@gmail.com>
2021-02-26 17:49:14 +0800

    x

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+
+    public static void main(String[] args) {
+        UnionFind uf = new UnionFind(11);
+        System.out.println(uf.find(9));
+        System.out.println(uf.find(10));
+        uf.union(9, 10);
+        uf.union(2, 3);
+        uf.union(2, 9);
+        uf.print();
+        System.out.println(uf.connected(3, 10));
+    }
+}
--- src/UnionFind.java
+++ src/UnionFind.java
@@ -1,2 +1,46 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class UnionFind {
+
+    int[] arr;
+    int count;
+
+    public UnionFind(int capacity) {
+        this.arr = new int[capacity];
+        this.count = capacity;
+        for (int i = 0; i < capacity; i++) {
+            arr[i] = i;
+        }
+    }
+
+    public void union(int a, int b) {
+        int aRoot = find(a);
+        int bRoot = find(b);
+        if (aRoot != bRoot) {
+            arr[aRoot] = bRoot;
+            count--;
+        }
+    }
+
+    public int find(int a) {
+        while (a != arr[a]) {
+            a = arr[a];
+        }
+        return a;
+    }
+
+    public boolean connected(int a, int b) {
+        return find(a) == find(b);
+    }
+
+    public int count() {
+        return this.count;
+    }
+
+    public void print() {
+        System.out.println(Arrays.toString(arr));
+    }
+
 }
